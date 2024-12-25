12ab66576660fc74ac9cb9427fe17f29095ba9f6
Muhammad Najeeb <yusnaj21@gmail.com>
2024-12-25 23:49:47 +0100

    Modified: Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,21 +3,24 @@ import Image from "next/image";
 import Link from "next/link";
 import MapImage from "../public/map.png";
 import Logo from "../public/logo.png";
+import { Button } from "./ui/button";
+import { Facebook, Instagram, Twitter, Music2 } from "lucide-react";
 const Footer = () => {
   return (
-    <div
-      className={`bg-[#2D4AF0] h-[300px] py-4 text-center text-[#fff] relative flex items-center`}
+    <footer
+      className={`bg-[#2D4AF0] h-[500px] py-4 text-[#fff] relative flex items-center`}
     >
       <div className="w-full absolute inset-0 bg-[#F0A500]/10"></div>
       <Image
         src={MapImage.src}
         alt="Empty Map"
-        width={500}
-        height={500}
-        className="absolute"
+        width={700}
+        height={700}
+        className="absolute object-cover"
       />
+
       <div className="absolute z-40 px-8 w-full flex flex-col md:flex-row justify-between items-center">
-        <div className="flex flex-col items-start justify-start text-left gap-y-4">
+        <div className="flex flex-col items-start justify-start gap-y-4 w-[40%]">
           <Image
             src={Logo.src}
             alt="IMS Company Logo"
@@ -29,35 +32,74 @@ const Footer = () => {
             getting better across the Nigeria and beyond
           </p>
           <p>We are located at Klen Plaza, Gwagwalada Abuja.</p>
+
+          <div>
+            <h4>Socials:</h4>
+            <div>
+              <Button variant="ghost" size="icon">
+                <Link href="https://twitter.com/ims_electrical" target="_blank">
+                  <Twitter />
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon">
+                <Link href="https://twitter.com/ims_electrical" target="_blank">
+                  <Facebook />
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon">
+                <Link href="https://twitter.com/ims_electrical" target="_blank">
+                  <Instagram />
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon">
+                <Link href="https://twitter.com/ims_electrical" target="_blank">
+                  <Music2 />
+                </Link>
+              </Button>
+            </div>
+          </div>
         </div>
 
-        <div>
-          <h4>Explore</h4>
-          <ul className="flex flex-col gap-y-4 items-start ml-2">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/services">Services</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
+        <div className="w-[33%] space-y-2">
+          <h4 className="font-bold text-xl">Have a complaint or suggestion?</h4>
+          <p className="text-sm">We are happy to hear from you</p>
+          <form className="text-[#000] flex flex-col gap-y-4" method="POST">
+            <div className="flex flex-col gap-y-2 items-start">
+              <label className="text-white" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example@email.com"
+                className="focus:ring-0 p-2 min-w-[400px] max-w-[400px]"
+              />
+            </div>
+            <div className="flex flex-col gap-y-2 items-start">
+              <label className="text-white" htmlFor="msg">
+                Message
+              </label>
+              <textarea
+                name="msg"
+                id="msg"
+                minLength={300}
+                maxLength={2000}
+                placeholder="Enter your message..."
+                className="max-h-[200px] min-h-[200px] min-w-[400px] max-w-[400px] p-2"
+              ></textarea>
+            </div>
 
-        <div>
-          <p>
-            With over 15 years in the Electrical Industry, Our Services keeps
-            getting better across the Nigeria and beyond
-          </p>
-          <p>We are located at Klen Plaza, Gwagwalada Abuja.</p>
+            <button
+              disabled
+              className="bg-[#F0A500] hover:bg-[#F0A500]/80 rounded-md px-4 py-2 text-[#000] w-full sm:w-1/3"
+            >
+              Send
+            </button>
+          </form>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
